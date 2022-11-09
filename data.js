21fc77f08dbc533aa18e00d4dc7ac927133d921f
Ryanator02 <fryanjames74@ou.edu>
2022-11-09 15:11:57 -0600

    added comments to app.js and data.js

--- data.js
+++ data.js
@@ -1,4 +1,13 @@
+//store users
 const userDB = [];
+
+//store pantry items
 var pantry = [];
 
+//store shopping list items
+var shoppingList = [];
+
+//export arrays
 module.exports = { userDB };
+module.exports = { pantry };
+module.exports = {shoppingList};
